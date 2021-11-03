e033dac43da45866a06d601eab42657c80e54721
ErickHernandez <erick.hernandez@unitec.edu>
2021-11-03 01:19:04 -0600

    Changed name resources

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,26 +1,26 @@
-resource "aws_lb" "playsms-web-alb" {
-  name               = "playsms-web-alb"
+resource "aws_lb" "proyectograd-web-alb" {
+  name               = "proyectograd-web-alb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = [aws_subnet.playsms-subnet-public-1.id, aws_subnet.playsms-subnet-public-2.id]
+  subnets            = [aws_subnet.proyectograd-subnet-public-1.id, aws_subnet.proyectograd-subnet-public-2.id]
   security_groups    = [aws_security_group.alb-http.id]
   tags = {
-    Name = "playsms-web-alb"
+    Name = "proyectograd-web-alb"
   }
 }
 
-resource "aws_lb_listener" "playsms-alb-listner" {
-  load_balancer_arn = "${aws_lb.playsms-web-alb.arn}"
+resource "aws_lb_listener" "proyectograd-alb-listner" {
+  load_balancer_arn = "${aws_lb.proyectograd-web-alb.arn}"
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.playsms-target-group.arn}"
+    target_group_arn = "${aws_lb_target_group.proyectograd-target-group.arn}"
   }
 }
 
-resource "aws_lb_target_group" "playsms-target-group" {
+resource "aws_lb_target_group" "proyectograd-target-group" {
   health_check {
     interval            = 10 # Approximate amount of time, in seconds, between health checks of an individual target
     path                = "/"
@@ -30,9 +30,9 @@ resource "aws_lb_target_group" "playsms-target-group" {
     unhealthy_threshold = 2 #Number of consecutive health check failures required before considering the target unhealthy
   }
 
-  name        = "playsms-target-group"
+  name        = "proyectograd-target-group"
   port        = 80
   protocol    = "HTTP"
   target_type = "instance"
-  vpc_id      = aws_vpc.playsms-vpc.id
+  vpc_id      = aws_vpc.proyectograd-vpc.id
 }
