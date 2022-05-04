251316a35b33c1830c17d5c3f8e7e368fab2e6d8
ErickHernandez <erick.hernandez@unitec.edu>
2022-05-04 00:13:49 -0600

    Now the server name is printed in the index for identifying the server that received the request from ALB

--- website/db.php
+++ website/db.php
@@ -3,9 +3,10 @@
 function get_db_credentials(){
     $cred = array();
 
-    $cred["name"] = "";
-    $cred["user"] = "";
-    $cred["pass"] = "";
+    $cred["host"] = "proyectograd-replica.c3vmmkmxglze.us-east-1.rds.amazonaws.com";
+    $cred["name"] = "prod";
+    $cred["user"] = "prod";
+    $cred["pass"] = "A4aShqST9L#";
 
     return $cred;
 }
